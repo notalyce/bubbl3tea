b350b82acb3df703f25aefde2bc350b764115b0b
Alyce Currier <notalyce@gmail.com>
2013-11-13 15:00:48 -0500

    Fresh Rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bubbl3tea::Application.config.secret_key_base = '81993349dfed036519016a1b786e62c7c0e59bbeadd6b9a580104863e7ada278a5e66e08d5ae440731df8aed07fc6d1af752b2e25eb78f33846cc59c01e15f39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bubbl3tea::Application.config.session_store :cookie_store, key: '_bubbl3tea_session'
