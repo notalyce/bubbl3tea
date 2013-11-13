4b03982f37def5a09a08a9ad9efa8de3a5cdaa6f
Alyce Currier <notalyce@gmail.com>
2013-11-13 15:08:43 -0500

    Generated posts controller + copied images and stylesheets from previous attempt.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Bubbl3tea::Application.routes.draw do
+
+  root 'posts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
