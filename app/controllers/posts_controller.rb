2b870845dbaf5acd303b9223db2bacaa8177b869
Alyce Currier <notalyce@gmail.com>
2013-11-13 15:42:35 -0500

    Got posts model set up.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,51 @@
 class PostsController < ApplicationController
+
+  before_filter :get_post, except: [:new, :create, :index]
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(params[:post].permit(:title, :excerpt, :body))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def index
+    @posts = Post.all(:order=> 'created_at desc')
+  end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :excerpt, :body)
+  end
+  
+  def get_post
+    @post = Post.find(params[:id])
+  end
+
 end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ActiveRecord::Base
+  # title
+  # body
+  # excerpt
+end
