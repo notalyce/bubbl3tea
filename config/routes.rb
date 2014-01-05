66baac36329da7828c2b3ff5f1a553bb57c392c1
Alyce Currier <notalyce@gmail.com>
2014-01-05 17:49:07 -0500

    Took out routes stuff for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Bubbl3tea::Application.routes.draw do
 
   resources :posts
-  get '/sign_up' => 'posts#index', :as => nil
+  # get '/sign_up' => 'posts#index', :as => nil
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
